b435e55bc2179f48ab9f58a468959ae6f899c394
Beth <bethhnielsen56@gmail.com>
2020-01-20 18:47:41 -0600

    wrote nuggetizer function

---
+++ functions.js
@@ -0,0 +1,12 @@
+console.log("functions!")
+ 
+const nuggetizer = (animal) => {
+    const output = `proceessed ${animal}`;
+    return output;
+};
+
+console.log(nuggetizer('chicken'));
+console.log(nuggetizer('pig'));
+console.log(nuggetizer('fish'));
+console.log(nuggetizer('cow'));
+console.log(nuggetizer('tire'));
