2cd736c7cd9f06b24f7680fa5285c3b0a7443527
Beth <bethhnielsen56@gmail.com>
2020-01-20 18:51:44 -0600

    wrote dog breed function

--- functions.js
+++ functions.js
@@ -10,3 +10,9 @@ console.log(nuggetizer('pig'));
 console.log(nuggetizer('fish'));
 console.log(nuggetizer('cow'));
 console.log(nuggetizer('tire'));
+
+const dogBreed = (dog) => {
+    return `My favorite dog breed a ${dog}`;
+}
+
+console.log(dogBreed('lab'));
